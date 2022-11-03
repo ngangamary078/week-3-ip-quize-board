e31dc7b5aa65e27d2c0d664ab449e35f54468b1c
Mary <ngangamary078@gmail.com>
2022-11-03 20:52:22 +0300

    added js some insertion

---
+++ js/quizboard.js
@@ -0,0 +1,14 @@
+const quizAnswers = document.querySelectorAll(".ouranswers");
+const submitAnswer = document.querySelector("#submit_ans");
+let marks = 0;
+quizAnswers.forEach(function (jibu) {
+    jibu.addEventListener("change", function (event) {
+        marks += parseInt(event.target.value);
+
+        //alert("Selected");
+    });
+});
+submitAnswer.addEventListener("click", function (event) {
+    alert("your score is " + marks);
+
+});
