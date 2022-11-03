f67d1e49ea735537c14238b342a075085a4084b3
Mary <ngangamary078@gmail.com>
2022-11-03 21:02:16 +0300

    added some insertion on js

--- js/quizboard.js
+++ js/quizboard.js
@@ -1,6 +1,7 @@
 const quizAnswers = document.querySelectorAll(".ouranswers");
 const submitAnswer = document.querySelector("#submit_ans");
 let marks = 0;
+let total = 0;
 quizAnswers.forEach(function (jibu) {
     jibu.addEventListener("change", function (event) {
         marks += parseInt(event.target.value);
